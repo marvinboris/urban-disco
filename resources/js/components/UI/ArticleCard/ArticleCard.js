0c0cfd584731fd1fd8289ed3771de9d745c5200c
Brainer21 <jarisl.ultio.21@gmail.com>
2021-08-15 21:15:40 +0100

    Fix on menu

--- resources/js/components/UI/ArticleCard/ArticleCard.js
+++ resources/js/components/UI/ArticleCard/ArticleCard.js
@@ -9,7 +9,11 @@ const articleCard = ({ src, title, subtitle, price = 0, link = '/', md = 6, lg =
         <Card className="text-dark shadow-sm">
             <CardImg top width="100%" src={src} alt="Card image cap" className="embed-responsive embed-responsive-4by3" />
             <CardBody className="text-center">
-                <CardTitle className="h5 text-700"><NavLink className="bg-dark-gradient bg-text" to={link.includes('//') ? link : '/products/' + link}>{title}</NavLink></CardTitle>
+                <CardTitle className="h5 text-700">
+                    {link.includes('//')
+                        ? <a className="bg-dark-gradient bg-text" href={link} target="_blank">{title}</a>
+                        : <NavLink className="bg-dark-gradient bg-text" to={'/products/' + link}>{title}</NavLink>}
+                </CardTitle>
                 <CardSubtitle className="text-muted text-truncate small mb-2">{subtitle}</CardSubtitle>
                 <CardText className="bg-dark-gradient bg-text h6 text-600">{price} FCFA</CardText>
             </CardBody>
